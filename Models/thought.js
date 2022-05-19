89cefc6d85b8cb94d695e300e7e7801ddeaf9807
sirmojosodope <stoverlee29@gmail.com>
2022-05-18 21:17:01 -0400

    adding Routes

--- Models/thought.js
+++ Models/thought.js
@@ -1,4 +1,26 @@
 const { Schema, model } = require("mongoose");
-const { thought } = require(".");
+const reactionSchema = require ('.reaction');
+const thought = model('thought', thoughtSchema);
+
+const thoughtSchema = new Schema(
+    {
+        throughText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now(),
+            get: 
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+)
 
 module.exports = thought;
---
+++ Routes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const apiRoutes = require('./Api');
+
+router.use('/api, apiRoutes');
+
+module.exports = router;
